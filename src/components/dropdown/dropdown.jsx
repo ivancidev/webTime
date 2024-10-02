b12e8e1907c6c060c6c6a6c8797bc83d62e7f0df
ivancidev <herlanherbas@gmail.com>
2024-10-02 18:46:18 -0400

    feat: Add component dropdown

---
+++ src/components/dropdown/dropdown.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Dropdown({
+  label,
+  options,
+  value,
+  onChange,
+  placeholder = "",
+  className = "",
+}) {
+  return (
+    <div>
+      {label && <label className={`block`}>{label}</label>}
+      <select value={value} onChange={onChange} className={className}>
+        <option value="">{placeholder}</option>
+        {options.map((option, index) => {
+          return (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+}
