63f56f8f19e72b2337459ec3f30807aa49efd01a
AndreaQuelali <andrealizbethquelali5@gmail.com>
2024-10-04 20:20:36 -0400

    refactor: update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
-import { Register } from "./features/register/pages/register";
+import { Register } from "./features/register/pages/register-books";
+import { router } from "./routes/router";
 
 function App() {
-  return (
-    <>
-      <Register />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
