015eb29b4a93c7d97f9a9e1e62eef132792d535b
ivancidev <herlanherbas@gmail.com>
2024-10-05 09:05:05 -0400

    Refactor register page layout to include footer buttons

--- src/features/register/pages/register.jsx
+++ src/features/register/pages/register.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import FormBook from "../components/form-book";
+import FooterButtons from "../components/footer-buttons";
 
 export const Register = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <FormBook />
+    <div className="flex min-h-screen flex-col">
+      <main className="flex-grow">
+        <FormBook />
+      </main>
+      <FooterButtons />
     </div>
   );
 };
