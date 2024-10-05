a9a1e2a04d122dc7d44c0c3272b5b0579c1b9b2c
ivancidev <herlanherbas@gmail.com>
2024-10-05 09:19:38 -0400

    Refactor register page layout to include navbar component

--- src/features/register/pages/register.jsx
+++ src/features/register/pages/register.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import FormBook from "../components/form-book";
 import FooterButtons from "../components/footer-buttons";
+import { Navbar } from "../components/navbar";
 
 export const Register = () => {
   return (
     <div className="flex min-h-screen flex-col">
+      <Navbar />
       <main className="flex-grow">
         <FormBook />
       </main>
