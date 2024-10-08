f81d08f6160a13a14c507f0b59c76f49e3b653cc
ivancidev <herlanherbas@gmail.com>
2024-10-08 07:43:27 -0400

    Refactor Card and Button components to use SVG icons and allow for optional image

--- src/features/register/pages/upload-files.jsx
+++ src/features/register/pages/upload-files.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import { Navbar } from "../components/navbar";
 import { Card } from "../components/card";
 import ImagePre from "../../../icons/imgPreview";
-import { Button } from "../../../components/buttons/button";
 import CancelIcon from "../../../icons/cancel";
 import UploadIcon from "../../../icons/upload";
 import FrontIcon from "../../../icons/front";
 import TextIcon from "../../../icons/text";
 import AudioIcon from "../../../icons/audio";
+import Button from "../../../components/buttons/button";
 export const Files = () => {
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
-      <section className="flex flex-col justify-center items-center">
-        <Card title="Imagen de portada" icon={FrontIcon} image={ImagePre} />
-        <Card title="Archivo PDF" icon={TextIcon} image={ImagePre} />
-        <Card title="Archivo de audio" icon={AudioIcon} />
+      <section className="flex flex-col justify-center items-center gap-10">
+        <Card title="Imagen de portada" SVG={FrontIcon} ImageSVG={ImagePre} />
+        <Card title="Archivo PDF" SVG={TextIcon} ImageSVG={ImagePre} />
+        <Card title="Archivo de audio" SVG={AudioIcon} />
       </section>
       <div className="flex w-full justify-end gap-4 mx-auto p-14">
         <Button text="Cancelar" variant="combCol2" SvgIcon={CancelIcon} />
