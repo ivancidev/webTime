abb70fad5d8cf3cd37e213dcbffe126f2a2f8be7
ivancidev <herlanherbas@gmail.com>
2024-10-08 21:14:58 -0400

    refactor: form-book component and footer-buttons component

--- src/features/register/components/footer-buttons.jsx
+++ src/features/register/components/footer-buttons.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-// import { Button } from "../../../components/buttons/button";
-
 import CancelIcon from "../../../icons/cancel";
 import ContinueIcon from "../../../icons/continue";
 import Button from "../../../components/buttons/button";
-import { useNavigate } from "react-router-dom";
 
-export default function FooterButtons() {
-  const navigation = useNavigate();
-
+export default function FooterButtons({ handleSubmit, onSubmit }) {
   const handleUpload = () => {
-    navigation("/upload-files");
+    handleSubmit(onSubmit)();
   };
+
   return (
     <footer className="flex w-full justify-end gap-4 mx-auto p-14">
       <Button text="Cancelar" variant="combCol2" SvgIcon={CancelIcon} />
--- src/features/register/components/form-book.jsx
+++ src/features/register/components/form-book.jsx
@@ -3,111 +3,124 @@ import { Dropdown } from "../../../components/dropdown/dropdown";
 import { useFetch } from "../../../hooks/useFetch";
 import { useForm } from "react-hook-form";
 import { InputText } from "../../../components/input/input";
+import FooterButtons from "../components/footer-buttons";
+import { useNavigate } from "react-router-dom";
 
 export default function FormBook() {
-  const { data: authorsData  } = useFetch("authors");
-  const { data: categoriesData} = useFetch("categories");
+  const { data: authorsData } = useFetch("authors");
+  const { data: categoriesData } = useFetch("categories");
   const { data: languagesData } = useFetch("languages");
 
-  const categories = categoriesData.categorias || []; 
-  const languages = languagesData.idiomas || []; 
-  const authors = authorsData.autores || []; 
-  
+  const categories = categoriesData.categorias || [];
+  const languages = languagesData.idiomas || [];
+  const authors = authorsData.autores || [];
+  const navigation = useNavigate();
+
   const [title, setTitle] = useState("");
 
-
-  const { register, handleSubmit, formState: { errors }, reset } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
     defaultValues: {
       title: "",
       author: "",
       category: "",
       language: "",
-      synopsis: ""
-    }
+      synopsis: "",
+    },
   });
 
   const onSubmit = (data) => {
-    console.log(data);
-    reset();
+    navigation("/upload-files");
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="w-[795px] mx-auto">
-      <h1 className="text-center text-secondary-sec2 m-[20px] font-title text-title-lg">
-        Formulario de Registro de Libro
-      </h1>
-      <div className="flex justify-between">
-        <InputText
-          name="title"
-          label="Titulo"
-          placeholder="Escribe aquí"
-          className="bg-transparent border-2 rounded border-[#F4EFF4] w-[340px] h-[50px] p-2 text-primary-pri3 font-body text-body-lg"
-          register={register}
-          errors={errors}
-          value = {title}
-          onChange={(e)=>setTitle(e.target.value)}
-        />
-        <Dropdown
-          name="author"
-          label="Autor"
-          options={authors}
-          placeholder="Seleccionar autor"
-          register={register}
-          errors={errors}
-          displayKey="autor"
-          valueKey="codAutor"
-        />
-      </div>
-      <div className="flex justify-between">
-        <Dropdown
-          name="category"
-          label="Categoría"
-          options={categories}
-          placeholder="Seleccionar categoría"
-          register={register}
-          errors={errors}
-          displayKey="nombreCategoria"
-          valueKey="codCategoria"
-        />
-        <Dropdown
-          name="language"
-          label="Idioma"
-          options={languages}
-          placeholder="Seleccionar idioma"
-          register={register}
-          errors={errors}
-          displayKey="idioma"
-          valueKey="codIdioma"
-        />
-      </div>
-      <div className="mt-7">
-        <label htmlFor="synopsis" className="py-1 text-primary-pri3 font-label text-label-lg">
-          Sinopsis
-        </label>
-        <textarea
-          id="synopsis"
-          name="synopsis"
-          className="w-[800px] h-[130px] bg-transparent border-2 rounded border-[#F4EFF4] p-2 text-primary-pri3 font-body text-body-lg mt-2"
-          placeholder="Escribe aquí"
-          {...register("synopsis", {
-            required: "La sinopsis no puede estar vacía",
-            pattern: {
-              value: /^[a-zA-Z0-9 .,!?;:()]*$/s,
-              message: "Solo se permiten caracteres alfanuméricos",
-            },
-            maxLength: {
-              value: 1000,
-              message: "No se permite más de 1000 caracteres",
-            },
-            minLength: {
-              value: 2,
-              message: "No se permite menos de 2 caracteres",
-            },
-          })}
-        />
-        {errors.synopsis && <span className="text-error-err2">{errors.synopsis.message}</span>}
-      </div>
-      <button type="submit" className="btn-submit text-white">Enviar</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)} className="w-[795px] mx-auto">
+        <h1 className="text-center text-secondary-sec2 m-[20px] font-title text-title-lg">
+          Formulario de Registro de Libro
+        </h1>
+        <div className="flex justify-between">
+          <InputText
+            name="title"
+            label="Titulo"
+            placeholder="Escribe aquí"
+            className="bg-transparent border-2 rounded border-[#F4EFF4] w-[340px] h-[50px] p-2 text-primary-pri3 font-body text-body-lg"
+            register={register}
+            errors={errors}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <Dropdown
+            name="author"
+            label="Autor"
+            options={authors}
+            placeholder="Seleccionar autor"
+            register={register}
+            errors={errors}
+            displayKey="autor"
+            valueKey="codAutor"
+          />
+        </div>
+        <div className="flex justify-between">
+          <Dropdown
+            name="category"
+            label="Categoría"
+            options={categories}
+            placeholder="Seleccionar categoría"
+            register={register}
+            errors={errors}
+            displayKey="nombreCategoria"
+            valueKey="codCategoria"
+          />
+          <Dropdown
+            name="language"
+            label="Idioma"
+            options={languages}
+            placeholder="Seleccionar idioma"
+            register={register}
+            errors={errors}
+            displayKey="idioma"
+            valueKey="codIdioma"
+          />
+        </div>
+        <div className="mt-7">
+          <label
+            htmlFor="synopsis"
+            className="py-1 text-primary-pri3 font-label text-label-lg"
+          >
+            Sinopsis
+          </label>
+          <textarea
+            id="synopsis"
+            name="synopsis"
+            className="w-[800px] h-[130px] bg-transparent border-2 rounded border-[#F4EFF4] p-2 text-primary-pri3 font-body text-body-lg mt-2"
+            placeholder="Escribe aquí"
+            {...register("synopsis", {
+              required: "La sinopsis no puede estar vacía",
+              pattern: {
+                value: /^[a-zA-Z0-9 .,!?;:()]*$/s,
+                message: "Solo se permiten caracteres alfanuméricos",
+              },
+              maxLength: {
+                value: 1000,
+                message: "No se permite más de 1000 caracteres",
+              },
+              minLength: {
+                value: 2,
+                message: "No se permite menos de 2 caracteres",
+              },
+            })}
+          />
+          {errors.synopsis && (
+            <span className="text-error-err2">{errors.synopsis.message}</span>
+          )}
+        </div>
+      </form>
+      <FooterButtons handleSubmit={handleSubmit} onSubmit={onSubmit} />
+    </div>
   );
 }
