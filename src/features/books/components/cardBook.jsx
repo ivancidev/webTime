ac87e2d30b90f87863dbba9f863347822971f207
AndreaQuelali <andrealizbethquelali5@gmail.com>
2024-10-08 23:11:39 -0400

    refactor: add border to card book

--- src/features/books/components/cardBook.jsx
+++ src/features/books/components/cardBook.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 export const CardBook = ({ titleBook, frontBook }) => {
   return (
-    <div className="w-60 h-80 bg-neutral-neu3 rounded-2xl flex flex-col items-center justify-center">
+    <div className="w-60 h-80 bg-neutral-neu3 rounded-2xl flex flex-col items-center justify-center hover:border-4 hover:border-secondary-sec1 transition duration-300 ease-in-out">
       <img src={frontBook} alt="" className="w-[220px] h-[270px] rounded-2xl" />
       <h3 className="font-title text-title-sm mt-2">{titleBook}</h3>
     </div>
