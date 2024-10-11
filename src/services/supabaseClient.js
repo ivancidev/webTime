07ed1be12fedc8e20e6859e76bf16225e0f43ef6
ivancidev <herlanherbas@gmail.com>
2024-10-11 13:50:07 -0400

    Refactor supabaseClient.js to use environment variables for Supabase URL and key

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,5 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://bumtrmbtbafwunrolrzy.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1bXRybWJ0YmFmd3Vucm9scnp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg2MTQyNzAsImV4cCI6MjA0NDE5MDI3MH0.D8aDaDrTnVLAh9amB_jhPCytrrDgemb9OBAC9xUItME';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
