41aa3e92e91447cff277724f60bbae5d9c2b701f
AndreaQuelali <andrealizbethquelali5@gmail.com>
2024-10-11 01:02:43 -0400

    feat: add icons for player

---
+++ src/icons/volume.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Volume() {
+  return (
+    <svg
+      width="41"
+      height="41"
+      viewBox="0 0 41 41"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M26.5475 14.4523C28.1488 16.0541 29.0484 18.2264 29.0484 20.4913C29.0484 22.7562 28.1488 24.9285 26.5475 26.5303M18.7917 8.5415L10.25 15.3748H3.41669V25.6248H10.25L18.7917 32.4582V8.5415Z"
+        stroke="#E6E1E5"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
