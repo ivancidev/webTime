e891428944b37d41600c872eb8168cea838941d4
Bruno Vega <brunovegavg.07@gmail.com>
2024-10-10 20:45:46 -0400

    feat: Add button to remove files

--- src/features/register/components/card.jsx
+++ src/features/register/components/card.jsx
@@ -1,54 +1,22 @@
-// src/components/card.jsx
 import React, { useState } from "react";
+import Button from "../../../components/buttons/button";
 
-export const Card = ({ title, SVG, ImageSVG, register, error, fileType, onFileChange }) => {
+export const Card = ({ title, SVG, register, error, fileType, onFileChange }) => {
   const [file, setFile] = useState(null);
 
   const handleFileChange = (event) => {
     const selectedFile = event.target.files[0];
     setFile(selectedFile);
-    onFileChange(event);  // Actualizar el estado en el formulario principal
+    onFileChange(event); // Actualizar el estado en el formulario principal
   };
-  const validationRules = {
-    required: "Este campo es obligatorio.",
-    validate: {
-      fileType: (fileList) => {
-        const file = fileList[0];
-        if (!file) return "Debe seleccionar un archivo.";
-  
-        switch (fileType) {
-          case "audio":
-            if (!(file.type === "audio/mpeg" || file.type === "audio/mp3" || file.name.endsWith(".mp3"))) {
-              return "Solo se permiten archivos de audio en formato .mp3";
-            }
-            if (file.size > 600 * 1024 * 1024) { // 600 MB
-              return "El archivo audio no puede exceder 600MB";
-            }
-            break;
-          case "pdf":
-            if (!(file.type === "application/pdf" || file.name.endsWith(".pdf"))) {
-              return "Solo se permiten archivos en formato .pdf";
-            }
-            if (file.size > 60 * 1024 * 1024) { // 60 MB
-              return "El archivo pdf no puede exceder 60MB";
-            }
-            break;
-          case "image":
-            if (!(file.type === "image/jpeg" || file.type === "image/png" || file.name.endsWith(".jpg") || file.name.endsWith(".png"))) {
-              return "Solo se permiten imágenes en formato .jpg o .png";
-            }
-            if (file.size > 5 * 1024 * 1024) { // 5 MB
-              return "El archivo imagen no puede exceder 5MB";
-            }
-            break;
-          default:
-            return "Formato de archivo no permitido.";
-        }
-      },
-    },
+
+  const removeFile = () => {
+    setFile(null);
+    onFileChange({ target: { name: register.name, files: [] } }); // Limpia el archivo en el formulario principal
   };
-  
+
   const validationAsterisk = <span className="text-error-err2">*</span>;
+
   return (
     <div className="w-[1000px] h-36 p-6 bg-transparent border border-neutral-neu2 rounded-[20px] flex flex-row justify-between">
       <div className="w-60 flex flex-col justify-center items-center">
@@ -61,21 +29,28 @@ export const Card = ({ title, SVG, ImageSVG, register, error, fileType, onFileCh
 
         <input
           type="file"
-          accept={ fileType === "audio" ? ".mp3" : fileType === "pdf" ? ".pdf" : "image/jpeg,image/png" }
+          accept={fileType === "audio" ? ".mp3" : fileType === "pdf" ? ".pdf" : "image/jpeg,image/png"}
           {...register}
-          onChange={(e) => {
-            handleFileChange(e); // Actualizar el estado local
-          }}
+          onChange={handleFileChange}
           className="hidden"
           id={`upload-${title}`}
         />
-
-        <label htmlFor={`upload-${title}`} className="cursor-pointer">
-          <span className="text-primary-pri3 bg-secondary-sec2 hover:bg-secondary-sec1 font-label rounded-[20px] h-10 pl-4 pr-5 text-label-sm text-center flex items-center">
+<div className="flex flex-row space-x-4 ">
+<label htmlFor={`upload-${title}`} className="cursor-pointer">
+          <span className="ml-40 whitespace-nowrap text-primary-pri3 bg-secondary-sec2 hover:bg-secondary-sec1 font-label rounded-[20px] h-10 pl-4 pr-5 text-label-sm text-center flex items-center">
             Elegir archivo
           </span>
         </label>
-        
+
+        <Button
+          type="button"
+          text="Quitar archivo"
+          onClick={removeFile}
+          disabled={!file} // Deshabilitado si no hay archivo seleccionado
+          variant="combCol1" // Usar un estilo de botón que prefieras
+        />
+</div>
+
         {error && (
           <p className="text-error-err2 text-sm mt-2">
             {error.message}
