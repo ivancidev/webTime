50e8930e9a1c4e2fc9dbe26383fbd87f6013fc82
AndreaQuelali <andrealizbethquelali5@gmail.com>
2024-10-10 20:19:20 -0400

    refactor: updating the values ​​of the workbook fields and image superimposed

--- src/features/books/pages/book-info.jsx
+++ src/features/books/pages/book-info.jsx
@@ -6,6 +6,16 @@ import ListenIcon from "../../../icons/listen";
 import ReadIcon from "../../../icons/read";
 
 export const BookInfo = () => {
+  const frontBook = "/src/assets/icons/logo.svg";
+  const titleBook = "HTML y CSS";
+  const autorBook = "Anonimo";
+  const categBook = "Fronted";
+  const lenguageBook = "Español";
+  const numPages = "3";
+  const duration = "5" + " min";
+  const synopsis =
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi, ipsa. Labore eveniet sint quidem reprehenderit, id distinctio laudantium similique dolores ex. Sapiente minima quam aliquid molestias a iusto sequi officiis.";
+
   return (
     <div className="flex min-h-screen flex-col ">
       <NavbarO />
@@ -15,13 +25,16 @@ export const BookInfo = () => {
         </Link>
       </div>
       <div className="flex flex-row justify-evenly">
-        <div className="w-[440px] h-[400px] bg-neutral-neu2 rounded-3xl">
-          <img src="" alt="" className="bg-secondary-sec2" />
+        <div className="relative w-[440px] h-[400px] bg-neutral-neu2 rounded-3xl">
+          <img
+            src={frontBook}
+            className=" w-72 h-[520px] rounded-3xl absolute inset-0 m-auto object-cover"
+          />
         </div>
         <div>
           <div className="flex flex-row justify-between">
             <h1 className="font-display text-display-lg text-secondary-sec2">
-              Titulo
+              {titleBook}
             </h1>
             <div className="flex flex-row space-x-8">
               <ListenIcon />
@@ -30,28 +43,32 @@ export const BookInfo = () => {
           </div>
 
           <div className="flex flex-col h-40 justify-between mt-8">
-            <h3 className="font-label text-label-lg text-neutral-neu2">
-              Autor:
-            </h3>
-            <h3 className="font-label text-label-lg text-neutral-neu2">
-              Idioma:
-            </h3>
-            <h3 className="font-label text-label-lg text-neutral-neu2">
-              Categoria:
-            </h3>
-            <h3 className="font-label text-label-lg text-neutral-neu2">
-              Número de páginas:
-            </h3>
-            <h3 className="font-label text-label-lg text-neutral-neu2">
-              Duración del audio:
-            </h3>
+            <div className="text-neutral-neu2 flex space-x-2">
+              <h3 className="font-label text-label-lg">Autor: </h3>
+              <h2 className="font-body text-body-lg mt-[2px]">{autorBook}</h2>
+            </div>
+            <div className="text-neutral-neu2 flex space-x-2">
+              <h3 className="font-label text-label-lg">Idioma: </h3>
+              <h2 className="font-body text-body-lg mt-[2px]">
+                {lenguageBook}
+              </h2>
+            </div>
+            <div className="text-neutral-neu2 flex space-x-2">
+              <h3 className="font-label text-label-lg">Categoria: </h3>
+              <h2 className="font-body text-body-lg mt-[2px]">{categBook}</h2>
+            </div>
+            <div className="text-neutral-neu2 flex space-x-2 ">
+              <h3 className="font-label text-label-lg">Número de páginas: </h3>
+              <h2 className="font-body text-body-lg mt-[2px]">{numPages}</h2>
+            </div>
+            <div className="text-neutral-neu2 flex space-x-2">
+              <h3 className="font-label text-label-lg">Duración del audio:</h3>
+              <h2 className="font-body text-body-lg mt-[2px]">{duration}</h2>
+            </div>
           </div>
           <div className="w-[500px] mt-8">
-            <p className="font-body text-body-lg text-neutral-neu2">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi,
-              ipsa. Labore eveniet sint quidem reprehenderit, id distinctio
-              laudantium similique dolores ex. Sapiente minima quam aliquid
-              molestias a iusto sequi officiis.
+            <p className="font-body text-body-md text-neutral-neu2">
+              {synopsis}
             </p>
           </div>
         </div>
