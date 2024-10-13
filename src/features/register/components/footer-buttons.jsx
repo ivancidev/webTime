0f62d273047aa5af9a991585fd0131627b793ac0
BrunoSC32 <132630771+BrunoSC32@users.noreply.github.com>
2024-10-13 11:29:40 -0400

    Revert "fix(registro-libro): mostrar el botón de cancelar en el formulario"

--- src/features/register/components/footer-buttons.jsx
+++ src/features/register/components/footer-buttons.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ContinueIcon from "../../../icons/continue";
-import CancelIcon from "../../../icons/cancel"; // Agrega esta línea
 import Button from "../../../components/buttons/button";
 
 export default function FooterButtons({ handleSubmit, onSubmit }) {
@@ -11,11 +10,6 @@ export default function FooterButtons({ handleSubmit, onSubmit }) {
   return (
     <footer className="flex w-full justify-end gap-4 mx-auto p-14">
       <Button
-        text="Cancelar" // Agrega este bloque
-        variant="combCol2" 
-        SvgIcon={CancelIcon} 
-      /> 
-      <Button
         text="Continuar"
         variant="combCol1"
         SvgIcon={ContinueIcon}
@@ -24,4 +18,3 @@ export default function FooterButtons({ handleSubmit, onSubmit }) {
     </footer>
   );
 }
-
