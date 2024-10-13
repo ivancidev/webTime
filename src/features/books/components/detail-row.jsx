e83226c44f7811d8f6c2bb462add0688e42c80d5
AndreaQuelali <andrealizbethquelali5@gmail.com>
2024-10-13 13:01:50 -0400

    style: update color component of black to white

--- src/features/books/components/detail-row.jsx
+++ src/features/books/components/detail-row.jsx
@@ -1,5 +1,5 @@
 export const DetailRow = ({ label, value }) => (
-  <div className="text-neutral-neu2 flex space-x-2">
+  <div className="text-primary-pri2 flex space-x-2">
     <h3 className="font-label text-label-lg">{label}: </h3>
     <h2 className="font-body text-body-lg mt-[2px]">
       {value || "Cargando..."}
