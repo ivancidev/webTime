50ceebedda7c961a8a4c7e6067a793bdfbf111c6
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2024-10-12 22:05:35 -0400

    refactor: SVG import paths to use correct case

--- src/features/books/components/carousel.jsx
+++ src/features/books/components/carousel.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { CardBook } from "./cardBook";
 import BeforeIcon from "../../../icons/before";
-import ButtonIcon from "../../../components/buttons/buttonIcon";
 import NextIcon from "../../../icons/next";
+import ButtonIcon from "../../../components/Buttons/buttonIcon";
 
 export const Carousel = ({ books }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -73,6 +73,7 @@ export const Carousel = ({ books }) => {
                 key={index}
                 titleBook={book.nombreLibro}
                 frontBook={book.enlacePortada}
+                book={book}
               />
             ))}
         </div>
