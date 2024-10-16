770775e2ef6a0404ab86bb21bfe773ebdafc76ce
AndreaQuelali <andrealizbethquelali5@gmail.com>
2024-10-15 23:39:55 -0400

    feat: create component text-large

---
+++ src/features/register/components/text-large.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+export const TextLarge = ({ sinopsis }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const maxLength = 250;
+
+  const toggleExpansion = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <div className="w-[500px] mt-8">
+      <p className="font-body text-body-md text-neutral-neu0 text-justify leading-6">
+        {isExpanded ? sinopsis : `${sinopsis.substring(0, maxLength)}...`}
+      </p>
+      <button
+        onClick={toggleExpansion}
+        className="text-secondary-sec2 font-body text-body-md hover:underline mt-2 text-justify"
+      >
+        {isExpanded ? "Leer menos" : "Leer más"}
+      </button>
+    </div>
+  );
+};
