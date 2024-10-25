2487ee4e95dff2f72cfc5dcbfdd603b2b76e75bb
Bruno Vega <brunovegavg.07@gmail.com>
2024-10-25 19:51:13 -0400

    feat: Add validation of the full name input

--- src/features/users/components/form-user.jsx
+++ src/features/users/components/form-user.jsx
@@ -1,44 +1,84 @@
+import React from "react";
+import { useForm } from "react-hook-form";
 import Button from "../../../components/buttons/button";
 import { InputText } from "../../../components/input/input";
 import ImageUploader from "./image-uploader";
 
 export const FormUser = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data); 
+  };
+
   return (
-    <div className="flex flex-col items-center">
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center">
       <h1 className="bg-gradient-to-r from-secondary-sec3 via-secondary-sec1 to-secondary-sec2 bg-clip-text text-transparent m-[20px] font-display text-display-md">
         ¡Bienvenido a Webtime!
       </h1>
       <ImageUploader />
+
       <div className="pb-6">
         <InputText
           name="name"
           label="Nombre completo"
-          placeholder="Escribe aquí"
-          className="w-96 bg-transparent border-[1px] rounded border-neutral-neu0 h-[50px] p-2 placeholder-neutral-neu0 text-primary-pri1  font-body text-body-md"
+          placeholder="Ingrese su nombre"
+          className="w-96 bg-transparent border-[1px] rounded border-neutral-neu0 h-[50px] p-2 placeholder-neutral-neu0 text-primary-pri1 font-body text-body-md"
+          register={register}
+          errors={errors}
+          validationRules={{
+            required: "Nombre no puede estar vacío",
+            pattern: {
+              value: /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]*$/,
+              message: "Nombre sólo admite caracteres a-z, A-Z",
+            },
+            minLength: {
+              value: 2,
+              message: "Nombre debe tener al menos 2 caracteres",
+            },
+            maxLength: {
+              value: 20,
+              message: "Nombre no debe exceder 20 caracteres",
+            },
+            validate: {
+              noMultipleSpaces: (value) =>
+                !/\s{2,}/.test(value) || "No se permiten espacios múltiples consecutivos",
+            },
+          }}
         />
         <InputText
           name="nickname"
           label="Nombre de usuario"
           placeholder="Escribe aquí"
-          className="w-96 bg-transparent border-[1px] rounded border-neutral-neu0 h-[50px] p-2 placeholder-neutral-neu0 text-primary-pri1  font-body text-body-md"
+          className="w-96 bg-transparent border-[1px] rounded border-neutral-neu0 h-[50px] p-2 placeholder-neutral-neu0 text-primary-pri1 font-body text-body-md"
+          register={register}
+          errors={errors}
         />
         <InputText
-          name="name"
+          name="email"
           label="Correo electrónico"
           placeholder="Escribe aquí"
           type="email"
-          className="w-96 bg-transparent border-[1px] rounded border-neutral-neu0 h-[50px] p-2 placeholder-neutral-neu0 text-primary-pri1  font-body text-body-md"
+          className="w-96 bg-transparent border-[1px] rounded border-neutral-neu0 h-[50px] p-2 placeholder-neutral-neu0 text-primary-pri1 font-body text-body-md"
+          register={register}
+          errors={errors}
         />
-
         <InputText
-          name="name"
+          name="password"
           label="Contraseña"
           placeholder="Escribe aquí"
           type="password"
-          className="w-96 bg-transparent border-[1px] rounded border-neutral-neu0 h-[50px] p-2 placeholder-neutral-neu0 text-primary-pri1  font-body text-body-md"
+          className="w-96 bg-transparent border-[1px] rounded border-neutral-neu0 h-[50px] p-2 placeholder-neutral-neu0 text-primary-pri1 font-body text-body-md"
+          register={register}
+          errors={errors}
         />
       </div>
-      <Button text="Registrarse" variant="combExp" />
-    </div>
+
+      <Button text="Registrarse" variant="combExp" type="submit" />
+    </form>
   );
 };
