f37ab6a4fcef7843d80da0882f560df8aa0bcc25
AndreaQuelali <andrealizbethquelali5@gmail.com>
2024-10-26 11:29:33 -0400

    feat: add buttonIcon close and add icon close

--- src/features/users/components/search-bar.jsx
+++ src/features/users/components/search-bar.jsx
@@ -1,29 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from "../../../icons/search";
+import Close from "../../../icons/closeS";
+import ButtonIcon from "../../../components/buttons/buttonIcon";
 export const SearchBar = () => {
+  const [searchText, setSearchText] = useState("");
+
+  const handleClear = () => {
+    setSearchText("");
+  };
   return (
     <div className="relative">
-      <div className="absolute right-0">
+      <div className="absolute left-0">
         <SearchIcon />
       </div>
       <input
         type="search"
         placeholder="Buscar"
-        className="w-96 h-12 rounded-3xl border-none px-4 pr-12 font-body text-body-md text-primary-pri2 bg-neutral-neu3 border-2 border-primary-pri2 focus:outline-none placeholder-neutral-neu0"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        className="w-96 h-12 rounded-3xl border-none px-4 pl-12 font-body text-body-md text-primary-pri2 bg-neutral-neu3 border-2 border-primary-pri2 focus:outline-none placeholder-neutral-neu0"
       />
-
-      <style jsx>{`
-        /* Ocultar el icono de borrado en navegadores WebKit (Chrome, Safari) */
-        input[type="search"]::-webkit-search-cancel-button {
-          -webkit-appearance: none; /* Para Chrome/Safari */
-          appearance: none; /* Para otros navegadores */
-        }
-
-        /* Ocultar el icono de borrado en Firefox */
-        input[type="search"]::-moz-search-cancel-button {
-          display: none; /* Para Firefox */
-        }
-      `}</style>
+      {searchText && (
+        <div className="absolute right-1 top-1">
+          <ButtonIcon
+            onClick={handleClear}
+            SvgIcon={Close}
+            variant="combColTrans2"
+          />
+        </div>
+      )}
     </div>
   );
 };
