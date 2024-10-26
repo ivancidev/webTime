c45c2b9caa4f606b54758c12fd432fd4f947d125
Bruno Vega <brunovegavg.07@gmail.com>
2024-10-26 10:47:55 -0400

    feat: Add validation of the image-uploader

--- src/features/users/components/image-uploader.jsx
+++ src/features/users/components/image-uploader.jsx
@@ -6,10 +6,23 @@ import UserReg from "../../../icons/userRegister";
 
 const ImageUploader = () => {
   const [image, setImage] = useState(null);
+  const [error, setError] = useState(null);
 
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
     if (file) {
+      const fileType = file.type;
+      const fileExtension = file.name.split(".").pop().toLowerCase();
+
+      if (
+        !((fileType === "image/png" && fileExtension === "png") ||
+          (fileType === "image/jpeg" && fileExtension === "jpg"))
+      ) {
+        setError("Solo se permiten archivos PNG o JPG (no JPEG).");
+        return;
+      }
+
+      setError(null);
       const imageUrl = URL.createObjectURL(file);
       setImage(imageUrl);
     }
@@ -17,6 +30,10 @@ const ImageUploader = () => {
 
   const handleImageDelete = () => {
     setImage(null);
+    setError(null); 
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
   };
 
   const fileInputRef = React.useRef(null);
@@ -28,32 +45,34 @@ const ImageUploader = () => {
   };
 
   return (
-    <div className="flex flex-row">
-      <div className="ml-10">
-        {image ? (
-          <img
-            src={image}
-            alt="Selected"
-            className="w-32 h-32 object-cover mb-2 rounded-full"
+    <div className="flex flex-col items-center">
+      <div className="flex flex-row">
+        <div className="ml-10">
+          {image ? (
+            <img
+              src={image}
+              alt="Selected"
+              className="w-32 h-32 object-cover rounded-full"
+            />
+          ) : (
+            <UserReg />
+          )}
+        </div>
+        <div className="flex flex-col justify-around ml-2 py-2">
+          <ButtonIcon SvgIcon={Camera} onClick={handleCameraClick} />
+
+          <input
+            type="file"
+            accept="image/*"
+            ref={fileInputRef}
+            onChange={handleImageUpload}
+            style={{ display: "none" }}
           />
-        ) : (
-          <UserReg />
-        )}
+
+          <ButtonIcon SvgIcon={Delete} onClick={handleImageDelete} />
+        </div>
       </div>
-
-      <div className="flex flex-col justify-around ml-2 py-2">
-        <ButtonIcon SvgIcon={Camera} onClick={handleCameraClick} />
-
-        <input
-          type="file"
-          accept="image/*"
-          ref={fileInputRef}
-          onChange={handleImageUpload}
-          style={{ display: "none" }}
-        />
-
-        <ButtonIcon SvgIcon={Delete} onClick={handleImageDelete} />
-      </div>
+      {error && <span className="text-error-err2">{error}</span>}
     </div>
   );
 };
