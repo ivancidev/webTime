b45515d690c968d369375232b092a46a38c5787a
SiAn20 <sidneyangellysg@gmail.com>
2024-10-28 20:25:03 -0400

    feat: make the search-bar component responsive

--- src/features/users/components/search-bar.jsx
+++ src/features/users/components/search-bar.jsx
@@ -18,7 +18,7 @@ export const SearchBar = () => {
         placeholder="Buscar"
         value={searchText}
         onChange={(e) => setSearchText(e.target.value)}
-        className="w-96 h-12 rounded-3xl border-none px-4 pl-12 font-body text-body-md text-primary-pri2 bg-neutral-neu3 border-2 border-primary-pri2 focus:outline-none placeholder-neutral-neu0"
+        className="w-[95%] sm:w-96 h-12 rounded-3xl border-none px-4 pl-12 font-body text-body-md text-primary-pri2 bg-neutral-neu3 border-2 border-primary-pri2 focus:outline-none placeholder-neutral-neu0"
       />
       {searchText && (
         <div className="absolute right-1 top-1">
