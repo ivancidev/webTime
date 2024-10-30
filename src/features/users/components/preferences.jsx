de8c2f7c92b3a58e44ce77b57dd52b9c88c6c8b2
AndreaQuelali <andrealizbethquelali5@gmail.com>
2024-10-29 21:52:29 -0400

    feat: add values of icons and texts to categories and times

--- src/features/users/components/preferences.jsx
+++ src/features/users/components/preferences.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import CardPref from './card-preference';
+import React from "react";
+import CardPref from "./card-preference";
 
-export default function Preferences( {text, svgIcons } ) {
-    return (
-        <section className='flex flex-col mt-10'>
-            <div>
-                <h1 className='mx-32 font-display text-display-sm text-secondary-sec1 text-gradient'>{text}</h1>
-            </div>
-            <div className={`flex flex-wrap gap-6 mx-36 mt-10 mr-[350px]`} >
-                {
-                    svgIcons.map(([text,icon], index) => (
-                        <div key={index} className=" mr-11">    
-                            <CardPref text={text} SvgIcon={icon}/>
-                        </div>
-                    ))
-                }
-            </div>
-        </section>
-    )
+export default function Preferences({ text, icons, variant, onSelect }) {
+  return (
+    <section className="flex flex-col mt-10">
+      <div>
+        <h1 className="ml-28 font-display text-display-sm bg-gradient-to-r from-secondary-sec3 via-secondary-sec1 to-secondary-sec2 bg-clip-text text-transparent">
+          {text}
+        </h1>
+      </div>
+      <div className={`flex flex-wrap gap-6 mx-36 mt-10`}>
+        {icons.map((iconText, index) => {
+          const contentText =
+            variant === "c"
+              ? iconText.nombreCategoria
+              : iconText.minutos + " minutos";
+          const contentIcon =
+            variant === "c"
+              ? iconText.enlace_icono_categoria
+              : iconText.enlace_icono_tiempo;
+          return (
+            <CardPref
+              key={index}
+              text={contentText}
+              icon={contentIcon}
+              onSelect={onSelect}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
 }
