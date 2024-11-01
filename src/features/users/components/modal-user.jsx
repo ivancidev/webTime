51ba60d33e801e4679c52f72a16b0afe277bf139
SiAn20 <sidneyangellysg@gmail.com>
2024-11-01 09:08:59 -0400

    feat: redirection with buttons

--- src/features/users/components/modal-user.jsx
+++ src/features/users/components/modal-user.jsx
@@ -4,8 +4,11 @@ import CloseIcon from "../../../icons/close";
 import LogOut from "../../../icons/logOut";
 import UserProf from "../../../icons/userProfile";
 import ViewProfile from "../../../icons/viewProfile";
+import { useNavigate } from 'react-router-dom';
 
 export const ModalUser = ({ codUser, nickname, imgUser, onClose }) => {
+  const navigate = useNavigate();
+
   return (
     <div className="w-52 h-[272px] rounded-xl border-[1px] border-primary-pri2 bg-primary-pri3 drop-shadow-lg">
       <div className="w-full flex justify-end">
@@ -25,8 +28,8 @@ export const ModalUser = ({ codUser, nickname, imgUser, onClose }) => {
           <UserProf />
         )}
         <h3 className="font-label text-label-lg">{nickname}</h3>
-        <Button text="Ver perfil" variant="combSize" SvgIcon={ViewProfile} />
-        <Button text="Cerrar sesión" variant="combSize" SvgIcon={LogOut} />
+        <Button text="Ver perfil" variant="combSize" SvgIcon={ViewProfile} onClick={() => navigate("/profile")}/>
+        <Button text="Cerrar sesión" variant="combSize" SvgIcon={LogOut} onClick={() => navigate("/")}/>
       </div>
     </div>
   );
