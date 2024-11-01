c144944262d49475629c79b889e7aa2edcb0c07b
SiAn20 <sidneyangellysg@gmail.com>
2024-11-01 08:51:47 -0400

    feat: Add transitions to navbar icons

--- src/components/navbar/navbarO.jsx
+++ src/components/navbar/navbarO.jsx
@@ -30,15 +30,15 @@ export const NavbarO = () => {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6"
-            stroke={isOpen ? "#0123FD" : "#AEAAAE"}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16m-7 6h7"
-              fill="currentColor"
-            />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 6h16M4 12h16m-7 6h7"
+            stroke={isOpen ? "#0123FD" : "#AEAAAE"}
+            className=" hover:stroke-secondary-sec2"
+          />
           </svg>
         </button>
       </div>
