f30186c58d25cff80e08d68669d4749237723c3d
SiAn20 <sidneyangellysg@gmail.com>
2024-11-02 14:49:22 -0400

    refactor: add components

---
+++ src/features/users/components/modal-menu.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+
+export const ModalMenu = () => {
+  return (
+    <div className="w-30 bg-primary-pri3 drop-shadow-2xl">
+      <ul className="flex flex-col items-start p-3 space-y-4 w-full">
+        <li>
+          <Link
+            to="/app"
+            className="font-label text-label-md text-secondary-sec1 hover:text-secondary-sec2"
+          >
+            Inicio
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="text-neutral-neu1 font-label text-label-md"
+          >
+            Categorias
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="text-neutral-neu1 font-label text-label-md"
+          >
+            Foros
+          </Link>
+        </li>
+      </ul>
+    </div>
+    );
+};
