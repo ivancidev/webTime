d88b57f5e4f84049afc5cebce2b0201100f9c387
AndreaQuelali <andrealizbethquelali5@gmail.com>
2024-11-05 20:05:32 -0400

    feat: add tags of Concluido in cardBook

--- src/features/books/components/cardBook.jsx
+++ src/features/books/components/cardBook.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useAudio } from "../../../context/audio-context";
-export const CardBook = ({ titleBook, frontBook, book }) => {
+export const CardBook = ({ titleBook, frontBook, book, isCompleted }) => {
   const navigate = useNavigate();
   const { setShowAudioPlay } = useAudio();
   const handleClick = () => {
@@ -12,9 +12,14 @@ export const CardBook = ({ titleBook, frontBook, book }) => {
   };
   return (
     <div
-      className="w-40 h-50 bg-primary-pri3 rounded-2xl flex flex-col justify-start border-[1px] border-primary-pri2 hover:bg-secondary-sec2 hover:bg-opacity-75 hover:text-primary-pri3 sm:w-60 sm:h-80 flex-none cursor-pointer"
+      className="relative w-40 h-50 bg-primary-pri3 rounded-2xl flex flex-col justify-start border-[1px] border-primary-pri2 hover:bg-secondary-sec2 hover:bg-opacity-75 hover:text-primary-pri3 sm:w-60 sm:h-80 flex-none cursor-pointer"
       onClick={handleClick}
     >
+      {isCompleted && (
+        <div className="absolute top-2 right-2 bg-secondary-sec2 text-primary-pri3 font-label text-label-sm  w-20 h-7 rounded-md z-10 flex items-center justify-center">
+          Concluido
+        </div>
+      )}
       <img
         src={frontBook}
         alt=""
