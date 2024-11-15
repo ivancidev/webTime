386bc423372b0d371fda55e5b08df50ae09085cd
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2024-11-15 15:33:31 -0400

    refactor: reorganize modal components and update import paths

--- src/layouts/root.jsx
+++ src/layouts/root.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import { NavbarO } from "../components/navbar/navbarO";
 import { AudioPlayer } from "../features/books/pages/audio-player";
 import { useAudio } from "../context/audio-context";
-import { ShowStreak } from "../components/modal/notification-show";
+import { ShowStreak } from "../modals/notification-show";
 
 export const Root = () => {
   const { showAudioPlay, setShowAudioPlay } = useAudio();
