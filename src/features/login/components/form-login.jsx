fdd2f91818159a37a70bd52f17b76334badab944
Bruno Vega <brunovegavg.07@gmail.com>
2024-11-14 23:58:26 -0400

    style: add background to login form

--- src/features/login/components/form-login.jsx
+++ src/features/login/components/form-login.jsx
@@ -3,7 +3,12 @@ import { Form } from "../components/form";
 export const FormLogin = () => {
     return (
         <div className="w-full h-screen flex justify-center items-center bg-gradient-to-r from-secondary-sec3 via-secondary-sec1 to-secondary-sec2">
-            <div className="w-[650px] h-[550px] flex flex-col justify-center items-center bg-primary-pri3 rounded-2xl">
+            <img
+                src="https://uygicxekmfgvxjhuqaor.supabase.co/storage/v1/object/public/imagenes/pexels-cottonbro-4064843.jpg"
+                alt="fondo"
+                className="absolute inset-0 w-full h-full object-cover opacity-70"
+            />
+            <div className="relative w-[650px] h-[550px] flex flex-col justify-center items-center bg-primary-pri3 rounded-2xl shadow-2xl z-10 border border-gray-200">
                 <h1 className="bg-gradient-to-r from-secondary-sec3 via-secondary-sec1 to-secondary-sec2 bg-clip-text text-transparent w-auto font-display text-display-md">
                     ¡Bienvenido a Webtime!
                 </h1>
