a645214b533c68da62ba549920c6e9bd9be248b1
Isac Poly Bolivar Puma <119465061+polycyllo@users.noreply.github.com>
2024-11-16 02:39:37 -0400

    refactor: coloque estructura para forum-comment page

--- src/features/users/pages/forum-comment.jsx
+++ src/features/users/pages/forum-comment.jsx
@@ -2,27 +2,55 @@ import ButtonIcon from "../../../components/buttons/buttonIcon";
 import { Comment } from "../components/comment";
 import Logo from "../../../assets/icons/logo.svg";
 import BackIcon from "../../../icons/back";
+import { NavbarO } from "../../../components/navbar/navbarO";
+
 
 export const ForumComment = () => {
     return (
       <div className="flex max-h-screen flex-col bg-primary-pri3">
-          <div className="sticky top-0 sm:relative flex items-center bg-transparent rounded-3xl ml-2 sm:ml-8 p-2 z-40">
-              <ButtonIcon SvgIcon={BackIcon} onClick={() => navigate(-1)} />
-          </div>
-            <h1 className="flex justify-start text-secondary-sec2 font-title text-title-lg mx-28">
-                Comentarios del foro
-            </h1>
-        <div className="flex flex-row mx-14 my-10">
-            <div className="flex justify-end relative w-2/5 max-w-[90%] bg-neutral-neu2 rounded-xl">
-                <img
-                    src={Logo}
-                    className="w-full rounded-3xl object-cover"
-                />
+        <NavbarO/>
+
+        <div className="sticky top-0 sm:relative flex items-center bg-transparent rounded-3xl ml-2 sm:ml-8 p-2 z-40">
+            <ButtonIcon SvgIcon={BackIcon} onClick={() => navigate(-1)} />
+        </div>
+
+        <div className="flex w-full">
+            <div>
+                <h1 className="text-secondary-sec2 font-title text-title-lg ">
+                    Comentarios del foro
+                </h1>
+
+                <div className="flex flex-row mx-14 my-10">
+                    <div className="flex justify-end relative w-2/5 max-w-[90%] bg-neutral-neu2 rounded-xl">
+                        <img
+                            src={Logo}
+                            className="w-full rounded-3xl object-cover"
+                        />
+                </div>
+            
+                </div>
             </div>
-            <div className="flex w-3/5 flex-row ml-14">
-                <Comment/>
+
+            <div>
+                <div className="flex flex-col h-[690px] overflow-y-scroll scrollbar-hide ml-10">
+                    <Comment/>
+                    <Comment/>
+                    <Comment/>
+                    <Comment/>
+                    <Comment/>
+                    <Comment/>
+                    <Comment/>
+                    <Comment/>
+                    <Comment/>
+                    <Comment/>
+                    <Comment/>
+                    <Comment/>
+                </div>
             </div>
         </div>
+
+            
+
       </div>
     );
   };
