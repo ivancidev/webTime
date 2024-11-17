6ef4f4aa266b3ff9eb2ca28d6f7c61e40650116c
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2024-11-17 15:58:06 -0400

    feat: create Forum component to display forums with loading and error handling

---
+++ src/features/forums/pages/forum.jsx
@@ -0,0 +1,39 @@
+import { CircularProgress } from "@mui/material";
+import { transformData } from "../../../utils/transform-data";
+import { SectionWithCards } from "../sections/section-with-cards";
+import { useQuery } from "@tanstack/react-query";
+import { forumsService } from "../../../services/forums-service";
+
+export const Forum = () => {
+  const {
+    data: forums,
+    isLoading: isLoadingForo,
+    isError: isErrorForo,
+  } = useQuery(["forums"], forumsService);
+
+  if (isLoadingForo) {
+    return (
+      <div className="flex flex-col justify-center items-center min-h-screen bg-primary-pri3">
+        <CircularProgress color="primary" size={100} />
+        <h2 className="mt-4 text-xl">Cargando foros...</h2>
+      </div>
+    );
+  }
+  if (isErrorForo) return <p>Ha ocurrido un error al cargar los foros</p>;
+
+  const sectionForums = transformData(forums);
+
+  return (
+    <>
+      {sectionForums.map((secion, index) => {
+        return (
+          <SectionWithCards
+            key={index}
+            category={secion.title}
+            cards={secion.cards}
+          />
+        );
+      })}
+    </>
+  );
+};
