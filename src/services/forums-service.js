b83283c44744fcc1570fd771bec6adf1b8d22ff7
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2024-11-17 15:56:51 -0400

    feat: add forums service to fetch forum data from Supabase

---
+++ src/services/forums-service.js
@@ -0,0 +1,10 @@
+import { supabase } from "./supabaseClient";
+
+export const forumsService = async () => {
+  const { data, error } = await supabase.from("foro").select("*");
+  if (error) {
+    throw new Error(error.message);
+  }
+  console.log(data)
+  return data;
+};
