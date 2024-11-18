1c039de429db6f09f67885bddcb3f72e65d92507
MarceVRC <marceloromerococa@gmail.com>
2024-11-18 19:46:35 -0400

    Responsive: Container category adjustment

--- src/features/forums/sections/section-with-cards.jsx
+++ src/features/forums/sections/section-with-cards.jsx
@@ -2,8 +2,8 @@ import { CardForo } from "../components/card-foro";
 
 export const SectionWithCards = ({ category, cards }) => {
   return (
-    <section className="container mx-auto my-10 px-5">
-      <h2 className="text-secondary-sec2 font-title text-title-md my-6">
+    <section className="container mx-auto my-10 px-4 sm:px-6 lg:px-8">
+      <h2 className="text-secondary-sec2 font-title text-title-md my-6 text-center sm:text-left">
         {category}
       </h2>
       <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
