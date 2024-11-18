218a40c6c7069423cb3634d752b9108fe8bd08e1
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2024-11-18 07:32:47 -0400

    feat: update CardForo component to use dynamic image URL and improve hover effects

--- src/features/forums/components/card-foro.jsx
+++ src/features/forums/components/card-foro.jsx
@@ -1,18 +1,16 @@
 export const CardForo = ({ imgUrl, title, description }) => {
   return (
-    <div className="flex flex-col w-[352px] min-h-[240px] h-full bg-neutral-neu4 rounded-lg">
+    <div className="flex flex-col w-[352px] min-h-[240px] h-full bg-neutral-neu4 rounded-lg hover:cursor-pointer hover:bg-secondary-sec3 hover:text-neutral-neu2">
       <div>
         <img
           className="w-full h-[160px] object-cover rounded-t-lg"
-          src={"https://reactjs.org/logo-og.png"}
+          src={imgUrl}
           alt={title}
         />
       </div>
       <div className="flex flex-col flex-grow p-5">
-        <h3 className="text-body-lg font-title font-semibold mb-4">{title}</h3>
-        <p className="font-body text-body-md text-neutral-neu0 leading-5">
-          {description}
-        </p>
+        <h3 className="text-body-lg font-title font-semibold mb-4 ">{title}</h3>
+        <p className="font-body text-body-md leading-5">{description}</p>
       </div>
     </div>
   );
