9d3bff032eedcf0edf833b3bc0d99be44ee5ae64
Bruno Salcedo <bruno.salcedo.ca@gmail.com>
2024-11-19 21:59:57 -0400

    feat(auth): enhance reset password flow with EmailSentModal

--- src/services/reset-password.js
+++ src/services/reset-password.js
@@ -9,12 +9,6 @@ export const sendVerificationCode = async (email) => {
       body: JSON.stringify({ email }),
     });
 
-    if (!response.ok) {
-      throw new Error(
-        "Error al enviar el código. Verifica el correo ingresado."
-      );
-    }
-
     console.log("Código enviado correctamente al servidor.");
   } catch (error) {
     throw error;
