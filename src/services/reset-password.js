11c3a12236ee38c906de7c71b8b46ac01bd2a833
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2024-11-25 08:42:26 -0400

    feat: update reset password flow to send email instead of verification code

--- src/services/reset-password.js
+++ src/services/reset-password.js
@@ -1,12 +1,12 @@
 const API_URL = import.meta.env.VITE_API_URL;
-export const sendVerificationCode = async (email) => {
+export const sendEmail = async (email, url) => {
   try {
     const response = await fetch(`${API_URL}/send-code`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email }),
+      body: JSON.stringify({ email, url }),
     });
 
     console.log("Código enviado correctamente al servidor.");
