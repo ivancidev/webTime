d45ddde329f557c5792ec966c8bcd15bb44608d5
MarceVRC <marceloromerococa@gmail.com>
2024-11-26 17:39:18 -0400

    feat: functionn deletebookfromcollection implementes and designed

--- src/services/delete-book-collection.js
+++ src/services/delete-book-collection.js
@@ -1,1 +1,7 @@
 import { supabase } from "./supabaseClient";
+export const deleteBookFromCollection = async (idColeccion, codLibro) => {
+    if (!idColeccion || !codLibro) {
+      console.error("El ID de la colección y del libro son requeridos.");
+      return { success: false, error: "Faltan parámetros requeridos." };
+    }
+  };
