57015ce83f645441dc3404aef5ff3ab89c5506a2
MarceVRC <marceloromerococa@gmail.com>
2024-11-26 17:41:31 -0400

    feat: if error happen show message

--- src/services/delete-book-collection.js
+++ src/services/delete-book-collection.js
@@ -9,6 +9,12 @@ export const deleteBookFromCollection = async (idColeccion, codLibro) => {
           .from("RegistroColeccion")
           .delete()
           .match({ idColeccion, codLibro });
+          if (error) {
+            console.error("Error al eliminar el libro de la colección:", error.message);
+            return { success: false, error: error.message };
+          }
+          console.log("Libro eliminado exitosamente de la colección:", data);
+          return { success: true, data };
       } catch (error) {
         console.error("Error inesperado al eliminar el libro:", error.message);
         return { success: false, error: error.message };
