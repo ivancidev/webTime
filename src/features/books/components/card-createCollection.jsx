c35391a0e5e1413e93069ecf55a0ba7458cdb5df
Isac Poly Bolivar Puma <119465061+polycyllo@users.noreply.github.com>
2024-11-26 16:41:46 -0400

    fix-add img to createCollection

--- src/features/books/components/card-createCollection.jsx
+++ src/features/books/components/card-createCollection.jsx
@@ -13,7 +13,13 @@ export const CardCreateCollection = ({
                 className="flex items-center w-full hover:text-secondary-sec2 cursor-pointer mt-3"
                 onClick={() => setshowCreateColectionk(true)}
             >
-                <div className="rounded-full w-20"></div>
+                <div
+                    className="w-[65px] h-[90px] flex justify-center items-center rounded-lg bg-gray-300"
+                    style={{ backgroundColor: "#D9D9D9" }} // Color plomo
+                >
+                    <span className="text-2xl font-bold text-gray-700">+</span>
+                </div>
+                <div className="rounded-full"></div>
                 <div className="flex flex-col">
                     <h3 className="mx-2 font-label text-center text-label-md mt-2 truncate px-1">
                         {collectionName}
