999e87eab18b86e4de47064daf225009211a7b5a
MarceVRC <marceloromerococa@gmail.com>
2024-11-26 17:40:12 -0400

    feat: deletebook coleection error control implented

--- src/services/delete-book-collection.js
+++ src/services/delete-book-collection.js
@@ -4,4 +4,13 @@ export const deleteBookFromCollection = async (idColeccion, codLibro) => {
       console.error("El ID de la colección y del libro son requeridos.");
       return { success: false, error: "Faltan parámetros requeridos." };
     }
+    try {
+        const { data, error } = await supabase
+          .from("RegistroColeccion")
+          .delete()
+          .match({ idColeccion, codLibro });
+      } catch (error) {
+        console.error("Error inesperado al eliminar el libro:", error.message);
+        return { success: false, error: error.message };
+      }
   };
