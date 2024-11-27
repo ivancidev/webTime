40ad11012cdd09e3f4b1f6d4cbf74ccf4a6d51d8
MarceVRC <marceloromerococa@gmail.com>
2024-11-26 21:08:41 -0400

    refact(collection): clic on swipper

--- src/features/collections/components/card-collection.jsx
+++ src/features/collections/components/card-collection.jsx
@@ -21,6 +21,7 @@ export const CardCollection = ({ collectionName, books , collectionId}) => {
         grabCursor={true}
         modules={[EffectCards]}
         className="w-60 h-[285px]"
+        onClick={handleCollectionClick}
       >
         {books.slice(0, 4).map((book, index) => (
           <SwiperSlide
