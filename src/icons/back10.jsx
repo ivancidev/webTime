359f4f9b02f1f334ff06e91034e7e3941cf3ffb4
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2024-11-29 22:45:45 -0400

    refactor: remove unnecessary React imports from icon components

--- src/icons/back10.jsx
+++ src/icons/back10.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Back10() {
   return (
     <svg
--- src/icons/burger.jsx
+++ src/icons/burger.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Burger() {
   return (
     <svg
